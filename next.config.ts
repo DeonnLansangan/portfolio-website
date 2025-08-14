1c294c954b76a04592ec01c0fcf0f9ee2a1ad2d2
DeonnLansangan <deonnlansangan@gmail.com>
2025-08-15 00:49:49 +0800

    add referrer policy and HSTS headers

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,14 @@ const nextConfig: NextConfig = {
             key: "X-Frame-Options",
             value: "DENY",
           },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
         ],
       },
     ];
